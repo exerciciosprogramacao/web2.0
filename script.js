79ec298bf0d2bb19d4b7fc15ec3743e908f0a8fd
exerciciosprogramacao <githubexercicios@gmail.com>
2025-09-30 14:08:47 -0300

    v1
    1 e 2 e 3

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("parag").innerText = "Aqui eu escrevo algo";
+
+const botao = document.getElementById("but");
+
+botao.addEventListener("click", () => {
+
+  alert("Teste de entrada da função");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function toggleDarkMode() {
+  document.body.classList.toggle('cyberpunk');
+}
+
+
+
